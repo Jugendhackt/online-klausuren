5baf695281a25490b4046ac1444b25302974f7a3
Felix <felixletsplay@outlook.de>
2020-03-20 15:02:00 +0100

    Fix Text Task

--- backend/src/main/java/org/jugendhackt/online_klausuren/tasks/TextTask.java
+++ backend/src/main/java/org/jugendhackt/online_klausuren/tasks/TextTask.java
@@ -2,11 +2,8 @@ package org.jugendhackt.online_klausuren.tasks;
 
 public class TextTask extends Task{
 
-    public final String solution;
-
-    public TextTask(String title, String description, int time, String solution) {
+    public TextTask(String title, String description, int time) {
         super(title, description, time);
         this.type = "TEXT";
-        this.solution = solution;
     }
 }
