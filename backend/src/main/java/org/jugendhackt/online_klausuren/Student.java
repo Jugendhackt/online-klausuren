fdcc375242da1814d37c9406f550c6f1b6df5e7b
zuvielSkill <franz.schlicht@gmail.com>
2020-03-21 14:30:05 +0100

    Fehler ausgebessert

--- backend/src/main/java/org/jugendhackt/online_klausuren/Student.java
+++ backend/src/main/java/org/jugendhackt/online_klausuren/Student.java
@@ -44,7 +44,7 @@ public class Student {
         PrintWriter pWriter = null;
         try {
             pWriter = new PrintWriter(new BufferedWriter(new FileWriter("Students_data.txt", true)));
-            pWriter.println(GLOBAL_VARS.gson.toJson(submission), session.getId());
+            pWriter.println(GLOBAL_VARS.gson.toJson(submission) + session.getId());
         } catch (IOException ioe) {
             ioe.printStackTrace();
         } finally {
